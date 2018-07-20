7dbb3c4241900d5ab25151455e36f2964f8e0516
drojf <drojfjord@gmail.com>
2018-07-20 17:59:20 +1000

    add class to kee p track of ID/line numbers, add class to read XML file

---
+++ SuiMerger/Line.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuiMerger
+{
+    public struct LineInfo
+    {
+        public int id;    //the line number in the file this line originally came from (if applicable), or the PS3 ID for the line
+        public string text;  //the actual string value of the line
+    }
+
+    //this class keeps track of each line's line number
+    public class LineTracker
+    {
+        protected List<LineInfo> lines;
+
+        public LineTracker()
+        {
+        }
+
+        protected void AddLineWithIndex(string lineToAdd, int lineID)
+        {
+            lines.Add(new LineInfo()
+            {
+                id = lineID,
+                text = lineToAdd,
+            });
+        }
+
+        public List<LineInfo> GetLineInfoList()
+        {
+            return lines;
+        }
+    }
+
+    public class LineTrackerPS3 : LineTracker
+    {
+        public LineTrackerPS3() : base()
+        {
+
+        }
+
+        void AddLine(string lineToAdd, int lineID)
+        {
+            AddLineWithIndex(lineToAdd, lineID);
+        }
+    }
+
+    public class LineTrackerMG : LineTracker
+    {
+        int lineCount;
+
+        public LineTrackerMG() : base()
+        {
+            lineCount = 0;
+        }
+
+        public void AddLine(string lineToAdd)
+        {
+            AddLineWithIndex(lineToAdd, lineCount);
+            lineCount += 1;
+        }
+    }
+}
--- SuiMerger/Program.cs
+++ SuiMerger/Program.cs
@@ -1,15 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace SuiMerger
 {
+    class PS3XMLReader
+    {
+        public static async Task TestReader(System.IO.Stream stream)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.Async = true;
+
+            using (XmlReader reader = XmlReader.Create(stream, settings))
+            {
+                while (await reader.ReadAsync())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            Console.WriteLine("Attributes of <" + reader.Name + ">");
+                            while (reader.MoveToNextAttribute())
+                            {
+                                Console.WriteLine(" {0}={1}", reader.Name, reader.Value);
+                            }
+
+                            break;
+                        case XmlNodeType.Text:
+                            Console.WriteLine("Text Node: {0}",
+                                     await reader.GetValueAsync());
+                            break;
+                        case XmlNodeType.EndElement:
+                            Console.WriteLine("End Element {0}", reader.Name);
+                            break;
+                        default:
+                            Console.WriteLine("Other node {0} with value {1}",
+                                            reader.NodeType, reader.Value);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            string XMLFilePath = @"C:\temp\sui\sui_full.xml";
+
+            FileStream fs = new FileStream(XMLFilePath, FileMode.Open);
+
+            Task result = PS3XMLReader.TestReader(fs);
+
+            Console.ReadLine();
+
+            return;
+
+            LineTrackerMG lt = new LineTrackerMG();
+            string fileToParse = "manga_gamer_example.txt";
+            
+            using (StreamReader sr = new StreamReader(fileToParse))
+            {
+                String line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    lt.AddLine(line);
+                }
+            }
+
         }
     }
 }
