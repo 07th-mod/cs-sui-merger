0d7b28e25184f4bac653ca15db4a2a06b3951ed2
drojf <drojfjord@gmail.com>
2018-07-21 11:07:11 +1000

    Add class to merge separate xml into one file (in the correct order...)

--- SuiMerger/Program.cs
+++ SuiMerger/Program.cs
@@ -3,11 +3,57 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml;
 
 namespace SuiMerger
 {
+    class XMLMerger
+    {
+        static string xmlCountPattern = @".*?_(\d+?).xml";
+        static Regex xmlCountRegex = new Regex(xmlCountPattern, RegexOptions.IgnoreCase);
+
+        //merges files in alphabetical order
+        public static void MergeFilesInFolder(string folder, string outputFilePath)
+        {
+            Dictionary<int, string> filePathDict = new Dictionary<int, string>();
+            int numFiles = 0;
+
+            string[] files = Directory.GetFiles(folder);
+            foreach (string filename in files)
+            {
+                Match match = xmlCountRegex.Match(filename);
+                if (!match.Success)
+                {
+                    throw new FormatException("XML filename does not adhere to regex pattern!");
+                }
+
+                int xmlCount = Convert.ToInt32(match.Groups[1].Value);
+                Console.WriteLine($"{filename}: {xmlCount}");
+
+                filePathDict.Add(xmlCount, Path.Combine(folder, filename));
+                numFiles += 1;
+            }
+
+            using (FileStream outputFile = File.Open(outputFilePath, FileMode.Create))
+            {
+                long bytesRead = 0;
+                for (int i = 0; i < numFiles; i++)
+                {
+                    string filePath = filePathDict[i];
+                    Console.WriteLine($"Writing {filePath}");
+                    byte[] fileAsBytes = File.ReadAllBytes(filePath);
+                    outputFile.Write(fileAsBytes, 0, fileAsBytes.Length);
+
+                    //save total bytes read for debugging
+                    bytesRead += fileAsBytes.Length;
+                }
+
+                Console.WriteLine($"Read {bytesRead} bytes, Wrote {outputFile.Position} bytes");
+            }
+        }
+    }
     class PS3XMLReader
     {
         public static async Task TestReader(System.IO.Stream stream)
@@ -51,6 +97,11 @@ namespace SuiMerger
     {
         static void Main(string[] args)
         {
+            XMLMerger.MergeFilesInFolder(@"c:\temp\sui_try_merge", @"c:\temp\sui_xml_merged.xml");
+
+            Console.ReadLine();
+
+            return;
             string XMLFilePath = @"C:\temp\sui\sui_full.xml";
 
             FileStream fs = new FileStream(XMLFilePath, FileMode.Open);
