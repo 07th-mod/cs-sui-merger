93bcc0753b5057ba3176d29d357ee4ec1ac05509
Daniel Wong <drojfjord@gmail.com>
2018-12-08 13:02:04 +1100

    Use windows line endings

--- SuiMerger/Config.cs
+++ SuiMerger/Config.cs
@@ -8,7 +8,7 @@ namespace SuiMerger
 {
     class Config
     {
-        public static char newline = '\n';
+        public static string newline = "\r\n";
 
         //enable to ignore any lines which consist only of japanese punctuation when performing diff operations
         public static bool DIFF_IGNORE_JAPANESE_PUNCTUATION_ONLY_LINES = false;
