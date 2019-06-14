32071ff376ee327c12d6fcc39a3dbf45adfebeb3
Daniel Wong <drojfjord@gmail.com>
2019-06-14 19:57:28 +1000

    Add forking script merger

---
+++ SuiMerger/ForkingScriptMerger.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SuiMerger
+{
+    class ForkingScriptMerger
+    {
+        Regex modCallScriptSectionRegex = new Regex(@"ModCallScriptSection\(\s*""([^""]+)""\s*,\s*""([^""]+)""", RegexOptions.IgnoreCase);
+
+        public List<string> MergeForkedScript(string scriptFolder, string fileName)
+        {
+            List<string> mergedScriptLines = new List<string>();
+
+            string scriptPath = Path.Combine(scriptFolder, fileName);
+            string[] allLines = File.ReadAllLines(scriptPath);
+
+            bool gotVoiceMatchingStartComment  = false;
+            foreach (string line in allLines)
+            {
+                // Add the line from the original script
+                mergedScriptLines.Add(line);
+                if(line.Trim() == "//VoiceMatching")
+                {
+                    gotVoiceMatchingStartComment = true;
+                }
+
+                //TODO: only want to insert the highest level censor script section!!!
+                bool gotBasicMatch = line.Contains("ModCallScriptSection");
+                Match match = modCallScriptSectionRegex.Match(line);
+                bool gotRegexMatch = match.Success && match.Groups.Count == 3;
+                if(gotBasicMatch && !gotRegexMatch)
+                {
+                    throw new Exception("Line contained ModCallScriptSection but regex was unable to match it!");
+                }
+
+                if(!gotRegexMatch)
+                {
+                    continue;
+                }
+
+                // We want to take the first ModCallSCriptSection of each chunk, whic is delimited by
+                //  //VoiceMatching and //VoiceMAtchingEnd
+                // To do this, only allow voice matching once per each "//VoiceMatching" delimiter
+                if (gotVoiceMatchingStartComment)
+                {
+                    gotVoiceMatchingStartComment = false;
+                    string subScriptFileNameWithExt = match.Groups[1].Value + ".txt";
+                    string subScriptFunctionName = match.Groups[2].Value;
+                    Console.WriteLine($"{subScriptFileNameWithExt} -> {subScriptFunctionName}");
+
+                    // Try to load the subscript file
+                    string subScriptPath = Path.Combine(scriptFolder, subScriptFileNameWithExt);
+
+                    // Add the lines from the sub-script
+                    mergedScriptLines.AddRange(GetScriptFunctionContent(subScriptPath, subScriptFunctionName));
+                }
+            }
+
+            return mergedScriptLines;
+        }
+
+        /// <summary>
+        /// Retrieves the script content of a function ("functionName") in a script file ("fileName")
+        /// Throws an exception indicating if the file or function couldn't be found
+        /// </summary>
+        /// <returns></returns>
+        public string[] GetScriptFunctionContent(string subScriptPath, string functionName)
+        {
+            if (!File.Exists(subScriptPath))
+            {
+                throw new Exception($"Couldn't find subscript at path {subScriptPath}");
+            }
+
+            string[] subScriptLines = File.ReadAllLines(subScriptPath);
+
+            Regex functionDefinitionStartRegex = new Regex(functionName + @"\s*\(\s*\)", RegexOptions.IgnoreCase);
+
+            // Try to find the subscript function in the subscript file
+            int line_i = 0;
+            int? functionDefinitionStart = null;
+            for(; line_i < subScriptLines.Count(); line_i++)
+            {
+                if(functionDefinitionStartRegex.Match(subScriptLines[line_i]).Success)
+                {
+                    functionDefinitionStart = line_i;
+                    break;
+                }
+            }
+            line_i++;
+
+            if(functionDefinitionStart == null)
+            {
+                throw new Exception($"Function {functionName}() couldn't be found in {subScriptPath}");
+            }
+
+            //the next line after the function definition start should be just a "{" - throw exception if it isn't
+            if(subScriptLines[line_i].Trim() != "{")
+            {
+                throw new Exception($"Expected '{{' after {functionName}() in {subScriptPath} but got {subScriptLines[line_i]}");
+            }
+            line_i++;
+
+            int firstContentLine = line_i;
+            int? lastContentLine = null;
+
+            //get all the lines until reaching a closing '}'. If another '{' on a line by itself is seen, then throw an exception as we don't handle parsing scopes properly
+            for (; line_i < subScriptLines.Count(); line_i++)
+            {
+                string currentLine = subScriptLines[line_i];
+                if(currentLine.Trim() == "{")
+                {
+                    throw new Exception($"Got a second lone '{{' in {functionName}() in {subScriptPath} - scope handling not implemented");
+                }
+                else if(currentLine.Trim() == "}")
+                {
+                    lastContentLine = line_i - 1;
+                    break;
+                }
+            }
+
+            // Check for missing closing brace
+            if (lastContentLine == null)
+            {
+                throw new Exception($"Did not find closing '}}' in {functionName}() in {subScriptPath}");
+            }
+
+            int contentLength = lastContentLine.Value - firstContentLine;
+            string[] content = new string[contentLength];
+
+            Array.Copy(subScriptLines, firstContentLine, content, 0, contentLength);
+
+            return content;
+        }
+
+    }
+}
